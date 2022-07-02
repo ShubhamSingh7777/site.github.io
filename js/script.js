1dcedc7b7fefa6cfb61a3bd0c6b321aa8c160329
Shubham <shubham.srathore07@gmail.com>
2022-07-02 21:22:37 +0530

    front-end-site

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/62b6fc737b967b117996753e/1g6dckaan';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
